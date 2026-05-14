17dc85e7c21cfe64c2090964b55b364b73b6b5c9
zhoujiaangyao <jiangyao.zhou@astronergy.com>
2026-05-14 20:25:18 +0800

    Squash merge issue 6312ef59: 展示Hello World

---
+++ frontend/src/components/HelloWorld.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface HelloWorldProps {
+  className?: string;
+}
+
+export function HelloWorld({ className }: HelloWorldProps) {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center p-6 rounded-2xl bg-brand/5 border border-brand/20",
+        className
+      )}
+    >
+      <h1 className="text-4xl font-black tracking-tight text-brand">
+        Hello World
+      </h1>
+    </div>
+  );
+}
